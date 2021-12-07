e5c007fb569f6b38c3beb3fc3bbbe2657c39ba52
Donna Lorenzo <dlorenzo24@hotmail.com>
2021-12-07 15:03:29 -0600

    validated trip dates for past dates

--- src/client/js/daysLeftToTrip.js
+++ src/client/js/daysLeftToTrip.js
@@ -6,7 +6,11 @@ function daysLeft(departure){
         const today = new Date();
         const depDate = new Date(departure);
         const difference = depDate.getTime() - today.getTime();
-        const daysDiff = Math.round(difference / (1000 * 3600 * 24));
+        let daysDiff = Math.round(difference / (1000 * 3600 * 24));
+        if(daysDiff < 0){
+            alert('Departure date chosen has already past!');
+            daysDiff = 0;
+        }
         const s = daysDiff < 2 ? '' : 's';
         return `(${daysDiff} day${s} left)`;
     }
