29022024990f0776ede29d62c0450334427c0611
Donna Lorenzo <dlorenzo24@hotmail.com>
2021-12-13 08:10:11 -0600

    installed service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,8 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 
 module.exports = merge(common, {
@@ -29,6 +31,7 @@ module.exports = merge(common, {
         }),
         new MiniCssExtractPlugin({
             filename: 'main.[contenthash].css'
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 })
